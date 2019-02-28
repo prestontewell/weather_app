45e31b0a97807f589740036d218bbe1f093a993c
prestontewell <preston@tewellphoto.com>
2019-02-28 11:53:48 -0600

    add routes city and current with corresponding methods and returns for end user

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -1,2 +1,11 @@
 class Api::CitiesController < ApplicationController
+  def city
+    render json: "Chicago: Second City"
+  end
+
+  def time
+    time = Time.now.strftime("%b %e, %l:%M %p")
+    render json: {Current: 'Time In Chicago',  Is: time}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  get '/city' => 'api/cities#city'
+  get '/current' => 'api/cities#time'
 end
